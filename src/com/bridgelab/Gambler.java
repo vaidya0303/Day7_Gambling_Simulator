64f83b74cabd34014a1c46710f39c162066a8d7a
vaidya0303 <vaidya0304@gmail.com>
2022-11-15 16:47:48 +0530

    Added : coded weather the gambler won or lost

--- src/com/bridgelab/Gambler.java
+++ src/com/bridgelab/Gambler.java
@@ -4,22 +4,37 @@ import java.util.Scanner; // import scanner class
 
 public class Gambler {
 
-    public static void main(String[] args) {
+   
+	static int AMOUNT_OF_STAKE=100;
+	static int BET_AMOUNT=1;
+	static int WON=1;
+	static int LOST=0;
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Starting amount of stake : "+AMOUNT_OF_STAKE);
+		 System.out.println("Amount to bet after every play : "+BET_AMOUNT);
+		 int play = (int) Math.floor(Math.random() * 10) % 2;
+		 System.out.println("Gamblers play:"+play);
+		 int InHandcash = AMOUNT_OF_STAKE;
+		 switch(play) {
+		 	case 1:
+		 		 InHandcash = InHandcash + BET_AMOUNT;
+		 		 System.out.println("Total current cash gambler has "+InHandcash);
+		 		 break;
+		 	case 2:
+		 		 InHandcash = InHandcash - BET_AMOUNT;
+		 		 System.out.println("Total current cash gambler has "+InHandcash);
+		 		 break;
+		 	default:
+		 		 System.out.println("default");
+		 }
+		 if(play == 1) {
+			 System.out.println("Player had won the first play");
+		 }
+		 else {
+			 System.out.println("Player had lost the first play");
+		 }
 
-        Scanner sc = new Scanner(System.in); // create scanner object
-
-        System.out.println("Enter amount of stake :");
-
-        int amount = sc.nextInt();
-
-        System.out.println("Enter amount to bet after every play :");
-
-        int bet = sc.nextInt();
-
-        System.out.println("Amount of stake is "+amount);
-        
-        System.out.println("Amount to bet after every game "+bet);
-
-    }
+	}
 
 }
